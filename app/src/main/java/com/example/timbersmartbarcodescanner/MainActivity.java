d4eecda2bb16819363e7e83bf05d13c5bd9ceb77
MattStevens567 <mattstevens567@gmail.com>
2020-08-18 21:50:14 +1200

    Initial Commit test

--- app/src/main/java/com/example/timbersmartbarcodescanner/MainActivity.java
+++ app/src/main/java/com/example/timbersmartbarcodescanner/MainActivity.java
@@ -3,13 +3,28 @@ package com.example.timbersmartbarcodescanner;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mButtonClear;
+    private TextView mEditTextBarcode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("test this is a test this is what a test has");
+
+        mEditTextBarcode = findViewById(R.id.editTextBarcode);
+
+
+
+        mButtonClear = findViewById(R.id.buttonClear);
+        mButtonClear.setOnClickListener((View v) -> {
+            mEditTextBarcode.setText("");
+        });
     }
 }
