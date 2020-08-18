443d945129a74d279aa51c2e8bcaea6bc54d1442
MattStevens567 <mattstevens567@gmail.com>
2020-08-18 21:40:54 +1200

    Initial Commit test

--- app/src/main/java/com/example/timbersmartbarcodescanner/MainActivity.java
+++ app/src/main/java/com/example/timbersmartbarcodescanner/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("balh");
+        System.out.println("test this is a test this is what a test has");
     }
 }
