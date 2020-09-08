b27950b22c4aede2029c5497f3a99c5425a76937
Keeghan Campbell <Keeghancampbell@gmail.com>
2020-09-09 11:34:16 +1200

    deleted email icon off activity_barcode_screen.xml
    Signed-off-by: Keeghan Campbell <Keeghancampbell@gmail.com>

--- app/src/main/java/com/example/timbersmartbarcodescanner/ActivityMain.java
+++ app/src/main/java/com/example/timbersmartbarcodescanner/ActivityMain.java
@@ -1,15 +1,26 @@
 package com.example.timbersmartbarcodescanner;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+
 public class ActivityMain extends AppCompatActivity {
 
-
+    ListView ActivityMainListViewStocktake;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ActivityMainListViewStocktake = findViewById(R.id.ActivityMainListViewStocktakes);
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("Stocktake 1");
+        arrayList.add("Stocktake 2");
+        arrayList.add("Stocktake 3");
+
+
     }
 }
