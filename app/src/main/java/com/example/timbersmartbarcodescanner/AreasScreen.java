5d94f762069134d078d5cd6d657a702b39fb1a3f
Keeghan Campbell <Keeghancampbell@gmail.com>
2020-09-10 16:15:14 +1200

    All 3 Screens added
    Signed-off-by: Keeghan Campbell <Keeghancampbell@gmail.com>

--- app/src/main/java/com/example/timbersmartbarcodescanner/AreasScreen.java
+++ app/src/main/java/com/example/timbersmartbarcodescanner/AreasScreen.java
@@ -8,11 +8,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class AreasScreen extends AppCompatActivity {
+    ArrayList<Area> sampleStockTakes = new ArrayList<>();
     private static final String TAG = "RowsScrean";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +25,6 @@ public class AreasScreen extends AppCompatActivity {
         Log.d(TAG, "onCreate: Started");
         ListView mListView = findViewById(R.id.rowListView);
         //Some Test Data for the meantime
-        ArrayList<Area> sampleStockTakes = new ArrayList<>();
         for (int i = 0; i < 50; i++){
             sampleStockTakes.add(new Area("Tyron is awesome: " + i));
         }
@@ -33,5 +35,21 @@ public class AreasScreen extends AppCompatActivity {
         Button back = findViewById(R.id.rowAddButton);
         back.setOnClickListener(view -> startActivity(new Intent(AreasScreen.this, ScanningScreen.class)));
 
+    }
+
+    public void AreaScreenDeleteAreaButton(View view) {
+        LinearLayout parent = (LinearLayout) view.getParent();
+        TextView child = (TextView) parent.getChildAt(0);
+        String item = child.getText().toString();
+        int position;
+        for (int i=0;i <sampleStockTakes.size();i++){
+            if (sampleStockTakes.get(i).getmAreaName().equals(item)){
+                sampleStockTakes.remove(i);
+                final ListView mListView = findViewById(R.id.rowListView);
+                AreaListAdapter test = new AreaListAdapter(AreasScreen.this, R.layout.row_area, sampleStockTakes);
+                mListView.setAdapter(test);
+            }
+
+        }
     }
 }
